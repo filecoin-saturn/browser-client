25ae950051e26aae6973859a61e4c51d0d16c3cc
Eric Guan <guanzo91@gmail.com>
2022-02-17 14:51:38 -0800

    use is-ipfs to extract cids from urls

--- src/sw/sw-core.js
+++ src/sw/sw-core.js
@@ -1,17 +1,35 @@
 import createDebug from 'debug'
 
-import { findCid } from './matcher'
+import isIPFS  from 'is-ipfs'
 
 const debug = createDebug('sw')
 
-
 addEventListener('fetch', event => {
-    const result = findCid(event)
-    if (result.cid) {
-        // intercept
+    const { url } = event.request
+    const cid = findCID(url)
+    if (cid) {
+        debug('yes cid!', cid, url)
+    } else {
+        debug('no cid!')
     }
 })
 
 addEventListener('error', err => {
     debug('sw err', err)
 })
+
+// Modified from https://github.com/PinataCloud/ipfs-gateway-tools
+function findCID (url) {
+    const splitUrl = url.split('/')
+    for (const split of splitUrl) {
+        if (isIPFS.cid(split)) {
+            return split
+        }
+        const splitOnDot = split.split('.')[0]
+        if(isIPFS.cid(splitOnDot)) {
+            return splitOnDot
+        }
+    }
+
+    return null
+}
