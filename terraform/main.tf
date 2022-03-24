48e8427e605c7287c00e550f28c9f14bf03e7d96
Eric Guan <guanzo91@gmail.com>
2022-03-23 17:17:44 -0700

    add default tag to terraform resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,8 +13,20 @@ provider "aws" {
   region              = "us-east-1"
   alias               = "us-east-1"
   allowed_account_ids = var.allowed_account_ids
+
+  default_tags {
+    tags = {
+      Environment = var.env
+    }
+  }
 }
 
 provider "aws" {
   allowed_account_ids = var.allowed_account_ids
+
+  default_tags {
+    tags = {
+      Environment = var.env
+    }
+  }
 }
